e886ce29dbaebaa27702a266a345f5c87552fec4
sangmama <sangzuoye@163.com>
2017-01-17 17:52:54 +0800

    第一次修改
    增加了这个是测试文本的内容

---
+++ js/scrollbottomEvent.js
@@ -0,0 +1,23 @@
+﻿/**
+* 依赖jQuery监听滚动底部触发事件
+**/
+;(function( $ ) {
+
+    var timerLoadInfo = null;
+
+    //自动开启监听
+    $(window).scroll(function(){
+        var scrollTop = $(this).scrollTop();
+        var scrollHeight = $(document).height();
+        var windowHeight = $(this).height();
+
+        if( scrollHeight - (scrollTop+windowHeight) < 70 ) {
+            //页面到达底部，可以加载更多微博数据
+            clearTimeout(timerLoadInfo);
+            timerLoadInfo = setTimeout(function() {
+                $(window).trigger('scrollToBottom');
+            }, 500);
+        }
+    });
+
+})( jQuery );
